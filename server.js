07bab1eb3bca1815107c224ddbd0fdef09ac158a
Cara R <cara.rosa.02@gmail.com>
2023-11-03 14:48:44 -0500

    imported sequelize connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,7 +16,7 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
 
-sequelize.sync({ force: false}).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 })
 
